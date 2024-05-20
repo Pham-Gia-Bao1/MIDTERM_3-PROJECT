55d72a30deef5f1386b4af4778083b927c602995
PhamGiaBao <phamgiabao123abc@gmail.com>
2024-05-20 11:21:30 +0700

    add user component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,26 @@ import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Search from "./components/users/Search";
+import User from "./components/users/User";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";// App.js
 const App = () => {
   return (
     <div className="App">
       <Router>
         <Navbar />
         <div className="container">
-          <h1>GitHub Users Data</h1>
           <Switch>
             <Route exact path="/" component={Search} />
+            <Route exact path="/about" component={About} />
+            {/* Add this line to specify the routing*/}
+            <Route exact path="/user/:id" component={User} />
+            <Route path="/*" component={NotFound}></Route>
           </Switch>
         </div>
       </Router>
     </div>
   );
 };
+
 export default App;
