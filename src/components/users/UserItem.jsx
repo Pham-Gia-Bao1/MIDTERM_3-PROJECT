10c6b4be0563ddcd0dd77a215c3d11b14ce87af6
PhamGiaBao <phamgiabao123abc@gmail.com>
2024-05-20 11:22:42 +0700

    update userItem component

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,4 +1,6 @@
+// UserItem.js
 import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 const UserItem = (props) => {
   const { login, avatar_url, html_url } = props.user;
   return (
@@ -11,9 +13,9 @@ const UserItem = (props) => {
       />
       <h3>{login}</h3>
       <div>
-        <a href="#" className="btn btn-dark btn-sm my-1">
+        <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
           More
-        </a>
+        </Link>
       </div>
     </div>
   );
